332edcdb13702948f6f008cde773f96970e6f006
Jacky Yeh <jackyyeh1999@gmail.com>
2022-03-17 22:21:34 +0800

    serial_multiplier working

--- CONV.v
+++ CONV.v
@@ -1,30 +1,34 @@
-
 `timescale 1ns/10ps
 
 module  CONV(
 	input		clk,
 	input		reset,
-	output		busy,	
-	input		ready,	
-			
+	output		busy,
+	input		ready,
+
 	output		iaddr,
-	input		idata,	
-	
+	input		idata,
+
 	output	 	cwr,
 	output	 	caddr_wr,
 	output	 	cdata_wr,
-	
+
 	output	 	crd,
 	output	 	caddr_rd,
 	input	 	cdata_rd,
-	
+
 	output	 	csel
 	);
+	parameter DATA_WIDTH = 20;
 
+	wire [DATA_WIDTH-1:0] kernal_input;
+	wire [DATA_WIDTH-1:0] grey_pixel;
+	reg [2:0] counter_reg;
+
+	always @(posedge clk)
+	begin
+		counter_reg <= reset ? 0 : counter_reg;
+	end
 
 
 endmodule
-
-
-
-
