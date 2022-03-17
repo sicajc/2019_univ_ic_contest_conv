2c90be40140530788d150978fda5e6bccff2602c
Jacky Yeh <jackyyeh1999@gmail.com>
2022-03-18 00:28:13 +0800

    FSM added

--- CONV.v
+++ CONV.v
@@ -1,41 +1,62 @@
 `timescale 1ns/10ps
 
-module  CONV(
-	input		clk,
-	input		reset,
-	output		busy,
-	input		ready,
+module CONV(input		clk,
+            input		reset,
+            output		busy,
+            input		ready,
+            output		iaddr,
+            input		idata,
+            output	 	cwr,
+            output	 	caddr_wr,
+            output	 	cdata_wr,
+            output	 	crd,
+            output	 	caddr_rd,
+            input	 	cdata_rd,
+            output	 	csel);
+    reg [3:0] conv_current_state,conv_next_state;
+    parameter IDLE       = 0;
+    parameter CAL        = 1;
+    parameter DONE       = 2;
+    parameter DATA_WIDTH = 20;
 
-	output		iaddr,
-	input		idata,
+    wire [DATA_WIDTH-1:0] kernal_input;
+    wire [DATA_WIDTH-1:0] grey_pixel;
+    reg [2:0] counter_reg;
 
-	output	 	cwr,
-	output	 	caddr_wr,
-	output	 	cdata_wr,
+    always @(posedge clk)
+    begin
+        counter_reg <= reset ? 0 : counter_reg+3;
+    end
 
-	output	 	crd,
-	output	 	caddr_rd,
-	input	 	cdata_rd,
+    always @(posedge clk)
+    begin
+        conv_current_state <= reset ? IDLE : conv_next_state;
+    end
 
-	output	 	csel
-	);
-	reg [3:0] conv_current_state,conv_next_state;
-	parameter IDLE = 0;
-	parameter DATA_WIDTH = 20;
+    always @(*)
+    begin
+        case(conv_current_state)
+            IDLE:
+            begin
 
-	wire [DATA_WIDTH-1:0] kernal_input;
-	wire [DATA_WIDTH-1:0] grey_pixel;
-	reg [2:0] counter_reg;
+            end
 
-	always @(posedge clk)
-	begin
-		counter_reg <= reset ? 0 : counter_reg+3;
-	end
+            CAL:
+            begin
 
-	always @(posedge clk )
-	begin
-		conv_current_state <= reset ? IDLE : conv_next_state;
-	end
+            end
+
+            DONE:
+            begin
+
+            end
+
+            default:
+            begin
+
+            end
+        endcase
+    end
 
 
 endmodule
